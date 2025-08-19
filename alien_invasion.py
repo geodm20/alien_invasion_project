9a1b7f4d6774e61cc70913396a04897ff4c8e214
George Matos <geodm20@gmail.com>
2025-08-18 21:54:13 -0400

    feat: add ship to alien_invasion.py and refactor event into a new function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,23 +16,29 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set background color.
         self.bg_color = (self.settings.bg_color)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch the keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
+        
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
